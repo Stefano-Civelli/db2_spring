3fddbfdaf14f3bd471c352d05debbb9cb0fda971
Stefano Civelli <stefano.civelli@gmail.com>
2022-02-12 11:43:13 +0100

    Initial commit

---
+++ src/main/java/it/polimi/db2_spring/Db2SpringApplication.java
@@ -0,0 +1,13 @@
+package it.polimi.db2_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db2SpringApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(Db2SpringApplication.class, args);
+   }
+
+}
---
+++ src/test/java/it/polimi/db2_spring/Db2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package it.polimi.db2_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db2SpringApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
